40124d068a31eee66d3258e02fbe023b9ac8074b
Kenneth Colon <kennethmanuel250@gmail.com>
2020-09-02 22:14:20 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -9,13 +9,13 @@ class Person
     self.partner = person
     person.partner = self
   end
-
-end
-
-class PartnerError < StandardError
+ 
+  # Add the following two lines:
+  class PartnerError < StandardError
   end
 end
  
-beyonce = Person.new("Beyonce")
+beyonce = 
+Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
