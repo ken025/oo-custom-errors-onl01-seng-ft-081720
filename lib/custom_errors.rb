23b0b519ebd8c7a6c6df72c940bb5812e5e0067b
Kenneth Colon <kennethmanuel250@gmail.com>
2020-09-02 22:14:51 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,16 +1,19 @@
 class Person
   attr_accessor :partner, :name
 
-  def initialize(name)
+    def initialize(name)
     @name = name
   end
-
+ 
   def get_married(person)
     self.partner = person
-    person.partner = self
+    if person.class != Person
+      raise PartnerError
+    else
+      person.partner = self
+    end
   end
  
-  # Add the following two lines:
   class PartnerError < StandardError
   end
 end
